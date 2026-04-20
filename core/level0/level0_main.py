fc78d574c040c52a434affc2ea7f0450c0a61ba5
Roderick Perez <roderickperezaltamar@gmail.com>
2026-04-20 22:29:53 +0200

    Elevate solar panel and apply white transparency colorkey

--- core/level0/level0_main.py
+++ core/level0/level0_main.py
@@ -33,6 +33,7 @@ def run(screen, clock, fonts):
         except: pass
         try:
             SPRITES[4] = pygame.image.load("assests/solarPanel_V1_64x32.png").convert_alpha()
+            SPRITES[4].set_colorkey((255, 255, 255))
         except: pass
         try:
             SPRITES[5] = pygame.image.load("assests/windTurbine.png").convert_alpha()
@@ -318,6 +319,7 @@ def run(screen, clock, fonts):
                     pygame.draw.polygon(iso_surf, (100, 150, 100), [t, r, b, l])
                 
                 offset_y = 0
+                if b_id == 4: offset_y = 5 # Float above surface
                 
                 rect = sprite.get_rect(centerx=int(cx), top=int(cy - tile_h / 2 - offset_y))
                 iso_surf.blit(sprite, rect)
